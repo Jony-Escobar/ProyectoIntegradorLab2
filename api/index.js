b250acf610ebc78634caca30be8e28a14b30eec0
Escobar Jonathan <jony.escobar14@gmail.com>
2024-11-01 17:09:07 -0300

    Revert "Quitamos meta, agregamos el middleware a la app"
    This reverts commit 2a5736e8c932096b3fa607732f99f87e1b202290.

--- api/index.js
+++ api/index.js
@@ -3,17 +3,6 @@ import express, { urlencoded } from 'express';
 import cors from 'cors'
 import usuarioRoutes from '../routes/usuario.routes.js';
 
-//Configurar CSP para vercel
-app.use(helmet.contentSecurityPolicy({
-    directives: {
-        defaultSrc: ["'self'"],
-        scriptSrc: ["'self'", "https://vercel.live", "https://vercel.com", "'unsafe-inline'"],
-        connectSrc: ["'self'", "https://vercel.live", "https://vercel.com", "*.pusher.com", "*.pusherapp.com"],
-        imgSrc: ["'self'", "https://vercel.live", "https://vercel.com", "*.pusher.com", "data:", "blob:"],
-        frameSrc: ["'self'", "https://vercel.live", "https://vercel.com"],
-    }
-}));
-
 //Variables
 const port = 3000;
 const app = express();
