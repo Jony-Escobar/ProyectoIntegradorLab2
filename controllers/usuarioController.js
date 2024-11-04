6114247dc77326bcf7ce6dc9438e962ad592fbf1
Escobar Jonathan <jony.escobar14@gmail.com>
2024-11-04 12:11:17 -0300

    Update usuarioController.js
    Probando incompatibilidad con render

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -20,8 +20,8 @@ class UsuarioController {
                 // Guardar en cookie
                 res.cookie('_token', token, {
                     httpOnly: true,
-                    //secure: process.env.NODE_ENV === 'production',
-                    //sameSite: 'strict',
+                    secure: process.env.NODE_ENV === 'production',
+                    sameSite: 'strict',
                     //maxAge: 1000 * 60 * 60 * 12 // 12 horas en milisegundos
                 });
 
